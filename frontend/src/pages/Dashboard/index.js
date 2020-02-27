ed9c82f4f481afb116aca0ba6ad2bf456c478e9f
Fellipe Neves <fellipe_emanoel@univem.edu.br>
2020-02-27 13:23:42 -0300

    Inicializando a exibição de solicitações em tempo real (websocket)

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,10 +1,35 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useMemo} from 'react';
 import {Link} from 'react-router-dom';
 import api from '../../services/api';
+import socketio from 'socket.io-client';
 import './styles.css';
 
 export default function Dashboard(){
     const [spots, setSpots] = useState([]);
+    const [requests, setRequests] = useState([]);
+
+    const user_id = localStorage.getItem('user');
+
+    //useMemo parecido com o useEffect, feito para so refazer a conexa com o socket quando o usuario mudar
+    const socket = useMemo(() => socketio("http://localhost:3333", {
+        query: {user_id}
+    }), [user_id]);   //Faz a conexao co o socket passando o id do user logado
+
+
+    useEffect(() => {
+        //Toda vez que receber alguma mensage do websocket, realiza a acao
+
+        // socket.on('hello', data => {
+        //     console.log(data);
+        // });
+
+        //socket.emit('omni', 'Stack');
+
+        socket.on('booking_request', data => {
+            console.log(data)
+            setRequests([...requests, data]);  //Copia todos os dados que ja tem na request e adiciona a nova no final
+        });
+    }, [requests, socket])
 
     useEffect(() => {
         async function loadSpots(){
@@ -20,6 +45,17 @@ export default function Dashboard(){
 
     return (
         <>
+        <ul className="notifications">
+            {requests.map(request => (
+                <li key={request._id}>
+                    <p>
+                        <strong>{request.user.email}</strong> está solicitando uma reserva em <strong>{request.spot.company}</strong> para a data <strong>{request.date}</strong>
+                    </p>
+                    <button className="accept">ACEITAR</button>
+                    <button className="reject">REJEITAR</button>
+                </li>
+            ))}
+        </ul>
             <ul className="spot-list">
                 {spots.map(spot => (
                     <li key={spot._id}>
